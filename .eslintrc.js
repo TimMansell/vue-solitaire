a47de14141a9dd500a20c157262e426b94743d8d
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-10 22:38:43 +1000

    disable elsint max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     semi: ['error', 'always'],
+    'max-len': 'off',
     'vue/html-closing-bracket-newline': 'off',
   },
   overrides: [
