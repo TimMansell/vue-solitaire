a715202c8c3d9a6c9f497b3d7f1e9f3d0672b42a
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-12 11:09:37 +1000

    highlight selected card

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,7 @@
 const getters = {
   shuffledCards: (state) => state.shuffledCards,
   boardCards: (state) => state.board.cards,
+  selectedCards: (state) => state.selected,
   aces: (state) => state.board.aces,
   rules: (state) => state.rules,
 };
