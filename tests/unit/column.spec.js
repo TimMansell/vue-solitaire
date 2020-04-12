04aa53b27e1e6b2de6a4ed4d62a02eb2896bc6ec
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-12 18:10:47 +1000

    column unit tests

--- tests/unit/column.spec.js
+++ tests/unit/column.spec.js
@@ -1,10 +1,61 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
 import Column from '@/components/Column.vue';
+import state from '@/store/state';
+import getters from '@/store/getters';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
 
 describe('Column.vue', () => {
+  let store;
+
+  const actions = {
+    moveKingToColumn: jest.fn(),
+  };
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state,
+      getters,
+      actions,
+    });
+  });
+
   it('matches snapshot', () => {
-    const wrapper = shallowMount(Column);
+    const wrapper = shallowMount(Column, { store, localVue });
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have correct props', () => {
+    const propsData = {
+      cards: [{
+        value: 'K',
+        suit: 'd',
+        order: 1,
+        visible: true,
+        clickable: false,
+      }],
+      columnNo: 1,
+    };
+
+    const wrapper = shallowMount(Column, {
+      store,
+      localVue,
+      propsData,
+    });
+
+    expect(wrapper.props().cards).toBe(propsData.cards);
+    expect(wrapper.props().columnNo).toBe(1);
+  });
+
+  it('should call store action "moveKingToColumn" when clicked', () => {
+    const wrapper = shallowMount(Column, { store, localVue });
+
+    wrapper.find('[data-test="column"]').trigger('click');
+
+    expect(actions.moveKingToColumn).toHaveBeenCalled();
   });
 });
