ab43e20bd7ddaef9c44904c3eefc2da9eb868166
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-12 13:46:51 +1000

    add version

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="app">
     <router-view />
+    <div class="app-version">
+      v{{ version }}
+    </div>
   </div>
 </template>
+
+<script>
+import { version } from '../package.json';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      version,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 body {
   margin: 0;
   background: green;
+}
+
+.app-version {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  color: white;
 }
 </style>
