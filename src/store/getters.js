ee2272c50bae7499eca9345478019a4bac6cdafc
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-17 20:12:35 +1000

    Remove deprecated code from vuex getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,7 @@
 const getters = {
-  // shuffledCards: (state) => state.shuffledCards,
   boardCards: (state) => state.board.cards,
-  // toMove: (state) => state.selectedCards[0],
   foundationCards: (state) => state.board.foundation,
   selectedCardId: (state) => state.selectedCardId,
-  // rules: (state) => state.rules,
 };
 
 export default getters;
