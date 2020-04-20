ba4d6e476f3b710f1dc4f4933d384fc2d4a29fab
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-20 13:27:29 +1000

    Install and configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,21 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/strongly-recommended',
-    '@vue/airbnb',
-  ],
-  plugins: [
-    'jest',
-  ],
+  extends: ['plugin:vue/strongly-recommended', '@vue/airbnb', 'plugin:prettier/recommended'],
+  plugins: ['jest'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    semi: ['error', 'always'],
-    'max-len': 'off',
     'vue/html-closing-bracket-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
