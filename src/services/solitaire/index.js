86ce0cb26207081f5ae77292cb416794fae134ac
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-20 16:11:11 +1000

    Migrate to ES6 class

--- src/services/solitaire/index.js
+++ src/services/solitaire/index.js
@@ -10,125 +10,150 @@ import {
 } from './validation';
 import { getSelectedCard, getLastCard, moveCardsFrom, moveCardsTo } from './helpers';
 
-const cardsArray = {
-  values: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
-  suits: ['c', 's', 'h', 'd'],
-};
+export default class Solitaire {
+  constructor() {
+    this.cards = {
+      values: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+      suits: ['c', 's', 'h', 'd'],
+    };
 
-const rules = {
-  columns: [7, 7, 7, 7, 6, 6, 6, 6],
-  foundationColumns: [1, 1, 1, 1],
-};
-
-const getFoundations = () => rules.foundationColumns;
-
-const shuffleCards = () => {
-  const { values, suits } = cardsArray;
-
-  const deck = values.flatMap((value, index) =>
-    suits.map((suit) => {
-      const card = {
-        id: `${index}${suit}`,
-        value,
-        order: index + 1,
-        suit,
-        visible: false,
-      };
-
-      return card;
-    })
-  );
-
-  const shuffledDeck = shuffle(deck);
-
-  return shuffledDeck;
-};
-
-const setBoard = (shuffledCards) => {
-  const showCards = (cards, offset = 0) =>
-    cards.map((card, index) => {
-      if ((index + offset) % 2 === 0) {
-        return {
-          ...card,
-          visible: true,
-        };
-      }
-
-      return card;
-    });
-
-  const dealtCards = rules.columns.map((column, columnIndex, array) => {
-    const startArray = array.slice(0, columnIndex);
-    const endArray = array.slice(0, columnIndex + 1);
-
-    const calcOffset = (accumulator, currentValue) => accumulator + currentValue;
-
-    const startIndex = startArray.reduce(calcOffset, 0);
-    const endIndex = endArray.reduce(calcOffset, 0);
-
-    const cards = shuffledCards.slice(startIndex, endIndex).map((shuffledCard, index) => {
-      const card = {
-        ...shuffledCard,
-        position: [columnIndex, index],
-      };
-
-      return card;
-    });
-
-    // Offset by one.
-    if (columnIndex > 3) {
-      return showCards(cards, 1);
-    }
-
-    return showCards(cards);
-  });
-
-  return dealtCards;
-};
-
-const checkValidCardMove = (selectedCardId, selectedColumn, board) => {
-  const selectedCard = getSelectedCard(board, selectedCardId);
-  const lastColumnCard = getLastCard(board, selectedColumn);
-
-  // Relaxed validation for K to empty column
-  if (!lastColumnCard) {
-    const isValidKing = isValidKingMove(selectedCard, lastColumnCard);
-
-    return isValidKing;
+    this.rules = {
+      columns: [7, 7, 7, 7, 6, 6, 6, 6],
+      foundationColumns: [1, 1, 1, 1],
+    };
   }
 
-  // General validation.
-  const isValidCard = isMoveValidCard(selectedCard, lastColumnCard);
-  const isValidSuit = isMoveValidSuit(selectedCard, lastColumnCard);
-  const isValidOrder = isMoveValidOrder(selectedCard, lastColumnCard);
-  const isValidColumn = isMoveValidColumn(selectedCard, lastColumnCard);
+  shuffleCards() {
+    const { values, suits } = this.cards;
 
-  return isValidCard && isValidSuit && isValidOrder && isValidColumn;
-};
+    const deck = values.flatMap((value, index) =>
+      suits.map((suit) => {
+        const card = {
+          id: `${index}${suit}`,
+          value,
+          order: index + 1,
+          suit,
+          visible: false,
+        };
 
-const moveCards = (selectedCardId, selectedColumn, cardsFrom, cardsTo) => {
-  const cardFromColumn = moveCardsFrom(selectedCardId, cardsFrom);
-  const cardsToColumn = moveCardsTo(selectedCardId, selectedColumn, cardsFrom, cardsTo);
+        return card;
+      })
+    );
 
-  return {
-    cardFromColumn,
-    cardsToColumn,
-  };
-};
+    const shuffledDeck = shuffle(deck);
 
-const checkValidFoundationMove = (selectedCardId, selectedColumn, board) => {
-  const selectedCard = getSelectedCard(board.cards, selectedCardId);
-  const isValidFoundationSuit = isMoveValidFoundationSuit(selectedCard, selectedColumn, board);
-  const isValidFoundationOrder = isMoveValidFoundationOrder(selectedCard, selectedColumn, board);
+    this.shuffledCards = shuffledDeck;
+  }
 
-  return isValidFoundationSuit && isValidFoundationOrder;
-};
+  setBoard(customDeck) {
+    const { columns } = this.rules;
 
-export {
-  shuffleCards,
-  getFoundations,
-  setBoard,
-  checkValidCardMove,
-  moveCards,
-  checkValidFoundationMove,
-};
+    if (customDeck) {
+      this.shuffledCards = customDeck;
+    }
+    const { shuffledCards } = this;
+
+    const showCards = (cards, offset = 0) =>
+      cards.map((card, index) => {
+        if ((index + offset) % 2 === 0) {
+          return {
+            ...card,
+            visible: true,
+          };
+        }
+
+        return card;
+      });
+
+    const dealtCards = columns.map((column, columnIndex, array) => {
+      const startArray = array.slice(0, columnIndex);
+      const endArray = array.slice(0, columnIndex + 1);
+
+      const calcOffset = (accumulator, currentValue) => accumulator + currentValue;
+
+      const startIndex = startArray.reduce(calcOffset, 0);
+      const endIndex = endArray.reduce(calcOffset, 0);
+
+      const cards = shuffledCards.slice(startIndex, endIndex).map((shuffledCard, index) => {
+        const card = {
+          ...shuffledCard,
+          position: [columnIndex, index],
+        };
+
+        return card;
+      });
+
+      // Offset by one.
+      if (columnIndex > 3) {
+        return showCards(cards, 1);
+      }
+
+      return showCards(cards);
+    });
+
+    this.boardCards = dealtCards;
+  }
+
+  setSelectedCard(id) {
+    this.selectedCardId = id;
+  }
+
+  removeSelectedCard() {
+    this.selectedCardId = null;
+  }
+
+  setMoveCards(selectedColumn, cardsFrom, cardsTo) {
+    const { selectedCardId } = this;
+    const cardFromColumn = moveCardsFrom(selectedCardId, cardsFrom);
+    const cardsToColumn = moveCardsTo(selectedCardId, selectedColumn, cardsFrom, cardsTo);
+
+    this.MovedCards = {
+      cardFromColumn,
+      cardsToColumn,
+    };
+  }
+
+  isValidCardMove(selectedColumn, board) {
+    const { selectedCardId } = this;
+
+    const selectedCard = getSelectedCard(board, selectedCardId);
+    const lastColumnCard = getLastCard(board, selectedColumn);
+
+    // Relaxed validation for K to empty column
+    if (!lastColumnCard) {
+      const isValidKing = isValidKingMove(selectedCard, lastColumnCard);
+
+      return isValidKing;
+    }
+
+    // General validation.
+    const isValidCard = isMoveValidCard(selectedCard, lastColumnCard);
+    const isValidSuit = isMoveValidSuit(selectedCard, lastColumnCard);
+    const isValidOrder = isMoveValidOrder(selectedCard, lastColumnCard);
+    const isValidColumn = isMoveValidColumn(selectedCard, lastColumnCard);
+
+    return isValidCard && isValidSuit && isValidOrder && isValidColumn;
+  }
+
+  isValidFoundationMove(selectedColumn, board) {
+    const { selectedCardId } = this;
+
+    const selectedCard = getSelectedCard(board.cards, selectedCardId);
+    const isValidFoundationSuit = isMoveValidFoundationSuit(selectedCard, selectedColumn, board);
+    const isValidFoundationOrder = isMoveValidFoundationOrder(selectedCard, selectedColumn, board);
+
+    return isValidFoundationSuit && isValidFoundationOrder;
+  }
+
+  getBoard() {
+    return this.boardCards;
+  }
+
+  getFoundations() {
+    return this.rules.foundationColumns;
+  }
+
+  getMovedCards() {
+    return this.MovedCards;
+  }
+}
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,6 @@
-import {
-  shuffleCards,
-  getFoundations,
-  setBoard,
-  checkValidCardMove,
-  moveCards,
-  checkValidFoundationMove,
-} from '@/services/solitaire';
+import Solitaire from '@/services/solitaire';
+
+const SolitaireService = new Solitaire();
 
 const actions = {
   initGame({ dispatch }) {
@@ -13,59 +8,76 @@ const actions = {
     dispatch('setFoundations');
   },
   setFoundations({ commit }) {
-    const foundationColumns = getFoundations();
+    const foundationColumns = SolitaireService.getFoundations();
 
     commit('SET_FOUNDATIONS', foundationColumns);
   },
   setBoard({ commit }) {
-    const shuffledCards = shuffleCards();
-    const board = setBoard(shuffledCards);
+    SolitaireService.shuffleCards();
+    SolitaireService.setBoard();
+
+    const board = SolitaireService.getBoard();
 
     commit('SET_BOARD', board);
   },
   restartGame({ commit }) {
     commit('RESTART_GAME');
   },
-  selectCard({ commit, state }, id) {
+  selectCard({ commit, state, dispatch }, id) {
     const { selectedCard } = state;
 
     if (selectedCard === id) {
-      commit('UNSELECT_CARD');
+      dispatch('unselectCard');
     } else {
+      SolitaireService.setSelectedCard(id);
+
       commit('SELECT_CARD', id);
     }
+  },
+  unselectCard({ commit }) {
+    SolitaireService.removeSelectedCard();
+    commit('UNSELECT_CARD');
   },
   setColumn({ commit }, columnNo) {
     commit('SET_COLUMN', columnNo);
   },
-  moveCardsToColumn({ commit, state }) {
-    const { selectedCardId, selectedColumn, board } = state;
-    const cardsToMove = moveCards(selectedCardId, selectedColumn, board.cards, board.cards);
+  moveCardsToColumn({ commit, state, dispatch }) {
+    const { selectedColumn, board } = state;
 
-    const isValidMove = checkValidCardMove(selectedCardId, selectedColumn, board.cards);
+    SolitaireService.setMoveCards(selectedColumn, board.cards, board.cards);
+
+    const isValidMove = SolitaireService.isValidCardMove(selectedColumn, board.cards);
 
     if (isValidMove) {
+      const cardsToMove = SolitaireService.getMovedCards();
+
       commit('MOVE_CARDS_TO_COLUMN', cardsToMove);
       commit('REMOVE_CARDS_FROM_COLUMN', cardsToMove);
     }
 
-    commit('UNSELECT_CARD');
+    dispatch('unselectCard');
   },
-  moveCardToFoundation({ commit, state }) {
-    const { selectedCardId, selectedColumn, board } = state;
-    const cardsToMove = moveCards(selectedCardId, selectedColumn, board.cards, board.foundation);
+  moveCardToFoundation({ commit, state, dispatch }) {
+    const { selectedColumn, board } = state;
 
-    const isValidMove = checkValidFoundationMove(selectedCardId, selectedColumn, board);
+    SolitaireService.setMoveCards(selectedColumn, board.cards, board.foundation);
+
+    const isValidMove = SolitaireService.isValidFoundationMove(selectedColumn, board);
 
     if (isValidMove) {
+      const cardsToMove = SolitaireService.getMovedCards();
+
       commit('MOVE_CARD_TO_FOUNDATION', cardsToMove);
       commit('REMOVE_CARDS_FROM_COLUMN', cardsToMove);
     }
 
-    commit('UNSELECT_CARD');
+    dispatch('unselectCard');
   },
   dealTestCards({ commit, dispatch }, deck) {
-    const board = setBoard(deck);
+    SolitaireService.setBoard(deck);
+
+    const board = SolitaireService.getBoard();
+
     commit('SET_BOARD', board);
 
     dispatch('setFoundations');
