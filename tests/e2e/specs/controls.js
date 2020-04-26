4520cb1dad0bf5a9742717ceac59be844a559ab0
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-04-26 16:07:17 +1000

    Update e2e test to test for selected card on new game

--- tests/e2e/specs/controls.js
+++ tests/e2e/specs/controls.js
@@ -15,11 +15,15 @@ describe('Controls', () => {
       cy.get('[data-test="card-Ah"]').click();
       cy.get('[data-test="foundation-0"]').click();
 
+      cy.get('[data-test="card-5s"]').click();
+
       cy.get('[data-test="new-game"]').click();
 
       cy.get('[data-test="foundation-0"]').within(() => {
         cy.get('[data-test="card-Ah"]').should('not.be.visible');
       });
+
+      cy.get('[data-test="card-5s"]').should('not.have.class', 'card--is-selected');
     });
   });
 
