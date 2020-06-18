ca47647d3110178ba42854a0c36d26ad18d0900a
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-06-18 16:30:09 +1000

    Buttons css improvements

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -34,25 +34,36 @@ export default {
 .btn {
   background: transparent;
   border: 2px solid #fff;
+  border-radius: 5px;
   padding: 0.5rem;
   color: #fff;
   transition: all 0.2s;
   font-weight: 700;
+  font-size: 0.8rem;
+  text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+
+  @media (min-width: $bp-sm) {
+    font-size: 0.9rem;
+  }
 
   &:hover {
     background: #027002;
     cursor: pointer;
   }
 
-  &:first-of-type {
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-    border-right: 0;
+  & + & {
+    border-left: 0;
   }
 
-  &:last-of-type {
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
+  &:last-of-type:not(:first-of-type) {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+
+  &:first-of-type:not(:last-of-type) {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
   }
 
   &--alt {
