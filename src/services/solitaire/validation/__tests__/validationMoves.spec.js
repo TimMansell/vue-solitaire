380aa6da92e709d3be5a1f7d4cd41fcf8dfb8458
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-07-09 17:11:16 +1000

    validation moves unit tests

---
+++ src/services/solitaire/validation/__tests__/validationMoves.spec.js
@@ -0,0 +1,286 @@
+import {
+  validateCard,
+  validateColumn,
+  validateFoundationMove,
+  validateFoundationPosition,
+} from '../index';
+
+describe('validation moves', () => {
+  describe('validateCard', () => {
+    describe('card', () => {
+      it('should be a valid card', () => {
+        const card1 = {
+          order: 9,
+          suit: 'c',
+        };
+        const card2 = {
+          order: 10,
+          suit: 'c',
+        };
+
+        const result = validateCard(card1, card2);
+
+        expect(result).toBe(true);
+      });
+
+      it('should be an invalid card', () => {
+        const card1 = {
+          order: 9,
+          suit: 'c',
+        };
+        const card2 = {
+          order: 8,
+          suit: 'd',
+        };
+
+        const result = validateCard(card1, card2);
+
+        expect(result).toBe(false);
+      });
+
+      it('should be a valid card order but invalid card suit', () => {
+        const card1 = {
+          order: 9,
+          suit: 'c',
+        };
+        const card2 = {
+          order: 10,
+          suit: 'd',
+        };
+
+        const result = validateCard(card1, card2);
+
+        expect(result).toBe(false);
+      });
+
+      it('should be a valid card suit but invalid card order', () => {
+        const card1 = {
+          order: 9,
+          suit: 'c',
+        };
+        const card2 = {
+          order: 8,
+          suit: 'c',
+        };
+
+        const result = validateCard(card1, card2);
+
+        expect(result).toBe(false);
+      });
+    });
+
+    describe('king', () => {
+      it('should be a valid king', () => {
+        const card1 = {
+          value: 'K',
+          suit: 'c',
+        };
+        const card2 = {};
+
+        const result = validateCard(card1, card2);
+
+        expect(result).toBe(true);
+      });
+
+      it('should be an invalid king', () => {
+        const card1 = {
+          value: 'K',
+          suit: 'c',
+        };
+        const card2 = {
+          value: '10',
+          suit: 'c',
+        };
+
+        const result = validateCard(card1, card2);
+
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  describe('validateColumn', () => {
+    it('should be a valid column', () => {
+      const card = {
+        id: 1,
+      };
+      const cards = [
+        {
+          id: 2,
+        },
+      ];
+
+      const result = validateColumn(card, cards);
+
+      expect(result).toBe(true);
+    });
+
+    it('should be an invalid column', () => {
+      const card = {
+        id: 1,
+      };
+      const cards = [
+        {
+          id: 1,
+        },
+      ];
+
+      const result = validateColumn(card, cards);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('validateFoundationMove', () => {
+    describe('card', () => {
+      it('should be a valid card', () => {
+        const card = {
+          order: 2,
+          suit: 'c',
+        };
+        const cards = [
+          {
+            order: 1,
+            suit: 'c',
+          },
+        ];
+
+        const result = validateFoundationMove(card, cards);
+
+        expect(result).toBe(true);
+      });
+
+      it('should be an invalid card', () => {
+        const card = {
+          order: 3,
+          suit: 'd',
+        };
+        const cards = [
+          {
+            order: 1,
+            suit: 'c',
+          },
+        ];
+
+        const result = validateFoundationMove(card, cards);
+
+        expect(result).toBe(false);
+      });
+
+      it('should be a valid card order but invalid card suit', () => {
+        const card = {
+          order: 2,
+          suit: 'c',
+        };
+        const cards = [
+          {
+            order: 1,
+            suit: 'd',
+          },
+        ];
+
+        const result = validateFoundationMove(card, cards);
+
+        expect(result).toBe(false);
+      });
+
+      it('should be a valid card suit but invalid card order', () => {
+        const card = {
+          order: 3,
+          suit: 'c',
+        };
+        const cards = [
+          {
+            order: 1,
+            suit: 'c',
+          },
+        ];
+
+        const result = validateFoundationMove(card, cards);
+
+        expect(result).toBe(false);
+      });
+    });
+
+    describe('ace', () => {
+      it('should be a valid ace', () => {
+        const card = {
+          value: 'A',
+          suit: 'c',
+        };
+        const cards = [];
+
+        const result = validateFoundationMove(card, cards);
+
+        expect(result).toBe(true);
+      });
+
+      it('should be an invalid ace', () => {
+        const card = {
+          value: 'A',
+          suit: 'c',
+        };
+        const cards = [
+          {
+            value: 'A',
+            suit: 'd',
+          },
+        ];
+
+        const result = validateFoundationMove(card, cards);
+
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  describe('validateFoundationPosition', () => {
+    it('should be a valid position', () => {
+      const card = {
+        id: 3,
+      };
+      const cards = [
+        [],
+        [
+          {
+            id: 5,
+          },
+          {
+            id: 8,
+          },
+          {
+            id: 3,
+          },
+        ],
+      ];
+
+      const result = validateFoundationPosition(card, cards);
+
+      expect(result).toBe(true);
+    });
+
+    it('should be an invalid position', () => {
+      const card = {
+        id: 3,
+      };
+      const cards = [
+        [],
+        [
+          {
+            id: 5,
+          },
+          {
+            id: 3,
+          },
+          {
+            id: 8,
+          },
+        ],
+      ];
+
+      const result = validateFoundationPosition(card, cards);
+
+      expect(result).toBe(false);
+    });
+  });
+});
