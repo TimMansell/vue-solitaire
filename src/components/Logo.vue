6e46cac77e07e6463675c94e033c75b1baab66d3
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-07-26 17:47:08 +1000

    create logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img :src="logo" width="150" />
+</template>
+
+<script>
+import logo from '@/assets/logo.svg';
+
+export default {
+  name: 'Logo',
+  data() {
+    return {
+      logo,
+    };
+  },
+};
+</script>
