e3c03f91c4e46af11954cfa6a1fe69d695e092fc
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-03 22:14:24 +1000

    set up graphql query for new game

---
+++ src/functions/create-game.js
@@ -0,0 +1,59 @@
+import fetch from 'node-fetch';
+import ApolloClient, { gql } from 'apollo-boost';
+import 'dotenv/config';
+
+const { FAUNA_ACCESS_TOKEN } = process.env;
+const URL = 'https://graphql.fauna.com/graphql';
+
+const mutation = gql`
+  mutation CreateGame($data: GameInput!) {
+    createGame(data: $data) {
+      date
+      lost
+      won
+      abandoned
+      time
+    }
+  }
+`;
+
+const variables = {
+  data: {
+    date: '2020-08-03',
+    won: false,
+    lost: false,
+    abandoned: false,
+    time: 300,
+  },
+};
+
+const client = new ApolloClient({
+  uri: URL,
+  fetch,
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${FAUNA_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
+
+// eslint-disable-next-line import/prefer-default-export, consistent-return
+export async function handler() {
+  try {
+    const body = await client.mutate({
+      mutation,
+      variables,
+    });
+
+    console.log({ body });
+
+    return {
+      statusCode: 200,
+      body: `${JSON.stringify(body)}`,
+    };
+  } catch (error) {
+    console.log({ error });
+  }
+}
