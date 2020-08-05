3e9a3ad89154dbe35e72d4a91250bbc206c84cf7
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-05 17:01:13 +1000

    set up test to grab total games played

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,16 @@
 <template>
-  <div data-test="board">
-    <Foundation />
-    <Columns />
+  <div>
+    <div>Total games played: {{ gamesPlayed }}</div>
+    <div data-test="board">
+      <Foundation />
+      <Columns />
+    </div>
   </div>
 </template>
 
 <script>
+import { gql } from 'apollo-boost';
+
 import Foundation from '@/components/Foundation.vue';
 import Columns from '@/components/Columns.vue';
 
@@ -14,6 +19,24 @@ export default {
   components: {
     Foundation,
     Columns,
+  },
+  data() {
+    return {
+      gamesPlayed: 0,
+    };
+  },
+  async created() {
+    const response = await this.$apollo.query({
+      query: gql`
+        query {
+          totalGames {
+            count
+          }
+        }
+      `,
+    });
+
+    this.gamesPlayed = response.data.totalGames.count;
   },
 };
 </script>
