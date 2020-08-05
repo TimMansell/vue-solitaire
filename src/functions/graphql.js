1323a49cbfb0e997f8ce8fb2fb46376b0c34ede4
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-05 17:29:21 +1000

    Add APOLLO_SERVER_CORS_ENABLED to enable cors for local development

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -3,7 +3,7 @@ import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
 import { client, formatQuery } from './graphql/db';
 
-const { FAUNA_INTROSPECTION, FAUNA_PLAYGROUND } = process.env;
+const { FAUNA_INTROSPECTION, FAUNA_PLAYGROUND, APOLLO_SERVER_CORS_ENABLED } = process.env;
 
 const server = new ApolloServer({
   typeDefs,
@@ -15,4 +15,19 @@ const server = new ApolloServer({
   introspection: FAUNA_INTROSPECTION,
 });
 
-exports.handler = server.createHandler();
+const cors = APOLLO_SERVER_CORS_ENABLED
+  ? {
+      cors: {
+        origin: '*',
+        credentials: true,
+      },
+    }
+  : {};
+
+const options = {
+  ...cors,
+};
+
+console.log({ options });
+
+exports.handler = server.createHandler(options);
