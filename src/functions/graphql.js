8af490f30217055f8b909a0cba048a615acbf89f
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-05 15:21:22 +1000

    create FAUNA_INTROSPECTION, FAUNA_PLAYGROUND to control graphql playground & schema introspection

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -3,13 +3,16 @@ import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
 import { client, formatQuery } from './graphql/db';
 
+const { FAUNA_INTROSPECTION, FAUNA_PLAYGROUND } = process.env;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context() {
     return { client, formatQuery };
   },
-  playground: true,
+  playground: FAUNA_PLAYGROUND,
+  introspection: FAUNA_INTROSPECTION,
 });
 
 exports.handler = server.createHandler();
