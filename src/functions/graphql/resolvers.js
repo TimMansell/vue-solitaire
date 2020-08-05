62dfb63b22a19f2f9d24bd54bad44cc11c8f6e5e
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-05 15:58:58 +1000

    create mutations

--- src/functions/graphql/resolvers.js
+++ src/functions/graphql/resolvers.js
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+
 // eslint-disable-next-line import/prefer-default-export
 export const resolvers = {
   Query: {
@@ -14,7 +16,78 @@ export const resolvers = {
 
       const body = await client.query({ query });
 
-      return body.data.totalGames;
+      const { totalGames } = body.data;
+
+      return totalGames;
+    },
+  },
+  Mutation: {
+    createGame: async (obj, args, context) => {
+      const { client, formatQuery } = context;
+
+      const mutation = formatQuery(`
+        mutation CreateGame($data: GameInput!) {
+          createGame(data: $data) {
+            date
+            lost
+            won
+            abandoned
+            time
+            _id
+          }
+        }
+      `);
+
+      const variables = {
+        data: {
+          date: format(new Date(), 'yyyy-MM-dd'),
+          won: false,
+          lost: false,
+          abandoned: false,
+          time: 0,
+        },
+      };
+
+      const body = await client.mutate({
+        mutation,
+        variables,
+      });
+
+      const { createGame } = body.data;
+      const { _id: id } = createGame;
+
+      return {
+        ...createGame,
+        id,
+      };
+    },
+    updateGame: async (obj, args, context) => {
+      const { client, formatQuery } = context;
+
+      const mutation = formatQuery`
+        mutation UpdateAGame($id: ID!, $data: GameInput!) {
+          updateGame(id: $id, data: $data) {
+            date
+            lost
+            won
+            abandoned
+            time
+          }
+        }
+      `;
+
+      const body = await client.mutate({
+        mutation,
+        variables: args,
+      });
+
+      const { updateGame } = body.data;
+      const { _id: id } = updateGame;
+
+      return {
+        ...updateGame,
+        id,
+      };
     },
   },
 };
--- src/functions/graphql/schema.js
+++ src/functions/graphql/schema.js
@@ -5,7 +5,26 @@ export const typeDefs = gql`
   type Query {
     totalGames: totalGames!
   }
+  type Mutation {
+    createGame(data: GameInput!): Game!
+    updateGame(id: ID!, data: GameInput!): Game
+  }
   type totalGames {
     count: Int
+  }
+  type Game {
+    id: ID!
+    date: String!
+    won: Boolean!
+    lost: Boolean!
+    abandoned: Boolean!
+    time: Int!
+  }
+  input GameInput {
+    date: String!
+    won: Boolean!
+    lost: Boolean!
+    abandoned: Boolean!
+    time: Int!
   }
 `;
