81190d608655dcc65827984c14e823d0375638cf
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-07 19:45:27 +1000

    Stats component unit tests

---
+++ src/components/__tests__/stats.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Stats from '@/components/Stats.vue';
+import state from '@/store/state';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe('Stats.vue', () => {
+  let store;
+
+  beforeEach(() => {
+    const getters = {
+      stats: () => ({
+        totalGames: 1,
+      }),
+    };
+
+    store = new Vuex.Store({
+      state,
+      getters,
+    });
+  });
+
+  it('matches snapshot', () => {
+    const wrapper = shallowMount(Stats, {
+      store,
+      localVue,
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should show 1 game played', () => {
+    const wrapper = shallowMount(Stats, {
+      store,
+      localVue,
+    });
+
+    expect(wrapper.text()).toContain('1');
+  });
+});
