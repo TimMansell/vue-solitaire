8149504add4922c62018e845da88aa63803fff0d
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-08 13:59:24 +1000

    remove vuex requirement from stats.spec.js unit test

--- src/components/__tests__/stats.spec.js
+++ src/components/__tests__/stats.spec.js
@@ -1,32 +1,14 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex from 'vuex';
+import { shallowMount } from '@vue/test-utils';
 import Stats from '@/components/Stats.vue';
-import state from '@/store/db/state';
-
-const localVue = createLocalVue();
-
-localVue.use(Vuex);
 
 describe('Stats.vue', () => {
-  let store;
-
-  beforeEach(() => {
-    const getters = {
-      stats: () => ({
-        totalGames: 1,
-      }),
-    };
-
-    store = new Vuex.Store({
-      state,
-      getters,
-    });
-  });
-
   it('matches snapshot', () => {
     const wrapper = shallowMount(Stats, {
-      store,
-      localVue,
+      computed: {
+        stats: () => ({
+          totalGames: 1,
+        }),
+      },
     });
 
     expect(wrapper).toMatchSnapshot();
@@ -34,8 +16,11 @@ describe('Stats.vue', () => {
 
   it('should show 1 game played', () => {
     const wrapper = shallowMount(Stats, {
-      store,
-      localVue,
+      computed: {
+        stats: () => ({
+          totalGames: 1,
+        }),
+      },
     });
 
     expect(wrapper.text()).toContain('1');
