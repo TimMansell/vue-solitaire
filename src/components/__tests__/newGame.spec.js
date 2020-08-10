929464a05f3cd80251d62764c442946dcd53833c
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-10 15:02:43 +1000

    fix broken newGame.spec.js test

--- src/components/__tests__/newGame.spec.js
+++ src/components/__tests__/newGame.spec.js
@@ -3,7 +3,13 @@ import NewGame from '@/components/NewGame.vue';
 
 describe('NewGame.vue', () => {
   it('matches snapshot', () => {
-    const wrapper = shallowMount(NewGame);
+    const wrapper = shallowMount(NewGame, {
+      computed: {
+        game: () => ({
+          id: 1,
+        }),
+      },
+    });
 
     expect(wrapper).toMatchSnapshot();
   });
