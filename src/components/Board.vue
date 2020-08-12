da45e7a4aa4e661549784adb13b4c859d3fcd283
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-12 16:40:24 +1000

    create GlobalStats.vue to show global player base game count

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,11 +5,13 @@
       <Foundation />
       <Columns />
     </div>
+    <GlobalStats />
   </div>
 </template>
 
 <script>
 import Stats from '@/components/Stats.vue';
+import GlobalStats from '@/components/GlobalStats.vue';
 import Foundation from '@/components/Foundation.vue';
 import Columns from '@/components/Columns.vue';
 
@@ -17,6 +19,7 @@ export default {
   name: 'Board',
   components: {
     Stats,
+    GlobalStats,
     Foundation,
     Columns,
   },
