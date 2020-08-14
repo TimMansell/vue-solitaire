2c5826259a50155e4c26d8510a6ad584d58e0a2e
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-14 13:54:45 +1000

    unit tests for user service

---
+++ src/services/user/user/__tests__/index.spec.js
@@ -0,0 +1,55 @@
+import { getLocalUser, getUser } from '../index';
+
+const mockId = 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7';
+
+jest.mock('@/services/db', () => ({
+  getAUser: () => ({
+    response: {
+      uid: mockId,
+    },
+  }),
+}));
+
+jest.mock('uuid', () => ({
+  v4: () => mockId,
+}));
+
+describe('User', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  describe('getLocalUser', () => {
+    it('should get user id from getLocalUserID', () => {
+      localStorage.setItem('luid', mockId);
+
+      const id = getLocalUser();
+
+      expect(id).toEqual(mockId);
+    });
+
+    it('should set user id from setLocalUserID', () => {
+      const id = getLocalUser();
+      const luid = localStorage.getItem('luid');
+
+      expect(id).toEqual(mockId);
+      expect(luid).toEqual(mockId);
+    });
+  });
+
+  describe('getUser', () => {
+    it('should get user id from getServerUserID', async () => {
+      localStorage.setItem('suid', mockId);
+
+      const id = await getUser();
+
+      expect(id).toEqual(mockId);
+    });
+
+    it('should get user id from getServerUserID', async () => {
+      const id = await getUser();
+
+      expect(id).toEqual(mockId);
+    });
+  });
+});
