f78adab696ca3140ed395c27106292b4809ab52d
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-19 16:42:26 +1000

    install cross-env and set graphql test server to mock data

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -4,7 +4,7 @@ import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
 import { client } from './graphql/db';
 
-const { FAUNA_INTROSPECTION, FAUNA_PLAYGROUND } = process.env;
+const { FAUNA_INTROSPECTION, FAUNA_PLAYGROUND, NODE_ENV } = process.env;
 
 const server = new ApolloServer({
   typeDefs,
@@ -15,6 +15,7 @@ const server = new ApolloServer({
   playground: FAUNA_PLAYGROUND,
   introspection: FAUNA_INTROSPECTION,
   validationRules: [depthLimit(1)],
+  mocks: NODE_ENV !== 'production',
 });
 
 exports.handler = server.createHandler();
