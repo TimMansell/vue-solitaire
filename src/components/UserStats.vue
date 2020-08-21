213f30681ec1ed3f07a91cc3ec389e6102dbbb07
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-08-21 16:09:11 +1000

    Rename Stats to UserStats

---
+++ src/components/UserStats.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Games played: <span data-test="stats">{{ userStats.gameNumber }}</span>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Stats',
+  computed: {
+    ...mapGetters(['userStats']),
+  },
+};
+</script>
