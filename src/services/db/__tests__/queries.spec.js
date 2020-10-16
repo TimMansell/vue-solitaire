35030da677e0e0e6f2047b60931a8b05f1196e1c
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-10-16 14:21:46 +1100

    db service unit test

--- src/services/db/__tests__/queries.spec.js
+++ src/services/db/__tests__/queries.spec.js
@@ -1,9 +1,10 @@
-import { getAUser } from '../queries';
+import { getAUser, getGlobalStats } from '../queries';
 
 jest.mock('../apollo', () => ({
   query: () => ({
     data: {
       getUser: 1,
+      globalStats: 1,
     },
   }),
 }));
@@ -11,6 +12,15 @@ jest.mock('../apollo', () => ({
 describe('DB service queries', () => {
   it('getAUser', async () => {
     const result = await getAUser(1);
+
+    expect(result).toEqual({
+      error: false,
+      response: 1,
+    });
+  });
+
+  it('getGlobalStats', async () => {
+    const result = await getGlobalStats();
 
     expect(result).toEqual({
       error: false,
