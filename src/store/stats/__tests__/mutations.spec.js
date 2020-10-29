a492b321be80cdd842bedccf3b712b338d2b37de
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-10-29 20:34:26 +1100

    unit tests for user stats store

--- src/store/stats/__tests__/mutations.spec.js
+++ src/store/stats/__tests__/mutations.spec.js
@@ -1,14 +1,37 @@
 import mutations from '../mutations';
 
-const { SET_GLOBAL_STATS } = mutations;
+const { SET_GLOBAL_STATS, SET_USER_STATS, SHOW_STATS } = mutations;
 
 describe('Stats', () => {
-  it('SET_USER_ID', () => {
+  it('SET_GLOBAL_STATS', () => {
     const globalStats = { count: 1 };
     const state = { globalStats };
 
     SET_GLOBAL_STATS(state, globalStats);
 
     expect(state.globalStats).toEqual(globalStats);
+  });
+
+  it('SET_USER_STATS', () => {
+    const fullUserStats = {
+      count: 4,
+      won: 1,
+      lost: 1,
+      completed: 2,
+    };
+    const state = { fullUserStats };
+
+    SET_USER_STATS(state, fullUserStats);
+
+    expect(state.fullUserStats).toEqual(fullUserStats);
+  });
+
+  it('SHOW_STATS', () => {
+    const showStats = true;
+    const state = { showStats };
+
+    SHOW_STATS(state, showStats);
+
+    expect(state.showStats).toEqual(showStats);
   });
 });
