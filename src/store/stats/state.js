dde71788a53491172335a8ed89abc38cd78574ba
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-10-29 21:39:03 +1100

    add default 0 state to fullUserStats

--- src/store/stats/state.js
+++ src/store/stats/state.js
@@ -1,7 +1,12 @@
 // Return state as function so we can reset it.
 const state = () => ({
   globalStats: {},
-  fullUserStats: {},
+  fullUserStats: {
+    won: 0,
+    lost: 0,
+    played: 0,
+    abandoned: 0,
+  },
   showStats: false,
 });
 
