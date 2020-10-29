ff9f4ef492de65fbfb7a046393d794811ee1c31c
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-10-29 17:40:04 +1100

    add show stats button to UserStats.vue

--- src/components/UserStats.vue
+++ src/components/UserStats.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
-    Games: <span data-test="stats">{{ userStats.gameNumber }}</span>
+    Games: <span data-test="stats">{{ userStats.gameNumber }}</span> (<Button
+      link
+      @click="toggleStats"
+    >
+      view stats </Button
+    >)
+    <UserStatsOverlay v-if="showStats" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
+import UserStatsOverlay from '@/components/UserStatsOverlay.vue';
+import Button from '@/components/Button.vue';
 
 export default {
   name: 'Stats',
+  components: {
+    UserStatsOverlay,
+    Button,
+  },
   computed: {
-    ...mapGetters(['userStats']),
+    ...mapGetters(['userStats', 'showStats']),
+  },
+  methods: {
+    ...mapActions(['toggleStats']),
   },
 };
 </script>
