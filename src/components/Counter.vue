8edba8cee4af7ae0ce163e8481bf539d646e55ce
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-11-09 16:57:20 +1100

    Create Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    {{ formattedCount }}
+  </span>
+</template>
+
+<script>
+import numeral from 'numeral';
+
+export default {
+  name: 'Counter',
+  props: {
+    number: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    formattedCount() {
+      const { number } = this;
+
+      return numeral(number).format('0,0');
+    },
+  },
+};
+</script>
