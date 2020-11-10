0a311800c20bb147d86613ef3f9899b16feefb5a
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-11-10 15:45:44 +1100

    pass graphql query through context so netlify funciton cant use that to query fauna

--- src/functions/graphql.js
+++ src/functions/graphql.js
@@ -1,4 +1,5 @@
 import { ApolloServer } from 'apollo-server-lambda';
+import { gql } from 'apollo-boost';
 import depthLimit from 'graphql-depth-limit';
 import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
@@ -9,8 +10,15 @@ const { FAUNA_INTROSPECTION, FAUNA_PLAYGROUND, NODE_ENV } = process.env;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context() {
-    return { client };
+  context({ event }) {
+    const { body } = event;
+    const { query } = JSON.parse(body);
+
+    const gqlQuery = gql`
+      ${query}
+    `;
+
+    return { client, query: gqlQuery };
   },
   playground: FAUNA_PLAYGROUND,
   introspection: FAUNA_INTROSPECTION,
