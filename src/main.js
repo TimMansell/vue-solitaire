5ef00b686fac3ad473aa68d2eec2f09ea027f739
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-11-16 15:59:02 +1100

    remove vue-js-modal

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue';
-import VModal from 'vue-js-modal';
 import App from './App.vue';
 import store from './store';
 
 Vue.config.productionTip = false;
-
-Vue.use(VModal);
 
 const app = new Vue({
   store,
