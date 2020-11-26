620f4b8a226bde58d301e034c216eef55a9f871e
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-11-27 10:46:51 +1100

    destructure response object

--- src/store/stats/actions.js
+++ src/store/stats/actions.js
@@ -5,27 +5,30 @@ const actions = {
     const { suid } = rootState.user;
 
     const { error, response } = await db.getStatsCount(suid);
+    const { userStats, globalStats } = response;
 
     if (!error) {
-      commit('SET_GLOBAL_STATS', { ...response.globalStats });
-      commit('SET_USER_STATS', { ...response.userStats });
+      commit('SET_GLOBAL_STATS', { ...globalStats });
+      commit('SET_USER_STATS', { ...userStats });
     }
   },
   async getGlobalStats({ commit }) {
     const { error, response } = await db.getGlobalStats();
+    const { globalStats } = response;
 
     if (!error) {
-      commit('SET_FULL_STATS', { ...response.globalStats });
-      commit('SET_GLOBAL_STATS', { ...response.globalStats });
+      commit('SET_FULL_STATS', { ...globalStats });
+      commit('SET_GLOBAL_STATS', { ...globalStats });
     }
   },
   async getUserStats({ commit, rootState }) {
     const { suid } = rootState.user;
     const { error, response } = await db.getUserStats(suid);
+    const { userStats } = response;
 
     if (!error) {
-      commit('SET_FULL_STATS', { ...response.userStats });
-      commit('SET_USER_STATS', { ...response.userStats });
+      commit('SET_FULL_STATS', { ...userStats });
+      commit('SET_USER_STATS', { ...userStats });
     }
   },
   toggleStats({ commit, state }) {
