e0149b7344671f92e2d971df82e839c4a50b1c6a
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-11-27 10:48:03 +1100

    create unit test for grabVariables

--- src/functions/graphql/__tests__/helpers.spec.js
+++ src/functions/graphql/__tests__/helpers.spec.js
@@ -1,18 +1,18 @@
 import { gql } from 'apollo-boost';
-import { grabQuery } from '../helpers';
+import { grabQuery, grabVariables } from '../helpers';
+
+const event = {
+  path: '/graphql',
+  httpMethod: 'POST',
+  queryStringParameters: {},
+  headers: {},
+  body:
+    '{"operationName":"GetUserStats","variables":{"uid":"1"},"query":"query GetUserStats($uid: String!) {\\n  userStats(uid: $uid) {\\n    count\\n    won\\n    lost\\n    completed\\n    __typename\\n  }\\n}\\n"}',
+  isBase64Encoded: false,
+};
 
 describe('Graphql helpers', () => {
   it('grabQuery', () => {
-    const event = {
-      path: '/graphql',
-      httpMethod: 'POST',
-      queryStringParameters: {},
-      headers: {},
-      body:
-        '{"operationName":"GetUserStats","variables":{"uid":"1"},"query":"query GetUserStats($uid: String!) {\\n  userStats(uid: $uid) {\\n    count\\n    won\\n    lost\\n    completed\\n    __typename\\n  }\\n}\\n"}',
-      isBase64Encoded: false,
-    };
-
     const toCompare = gql`
       query GetUserStats($uid: String!) {
         userStats(uid: $uid) {
@@ -26,6 +26,14 @@ describe('Graphql helpers', () => {
     `;
 
     const result = grabQuery(event);
+
+    expect(result).toEqual(toCompare);
+  });
+
+  it('grabVariables', () => {
+    const toCompare = { uid: '1' };
+
+    const result = grabVariables(event);
 
     expect(result).toEqual(toCompare);
   });
