da3a8e8a87685e7bd5fd11949b20df956fd13b97
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-11-27 17:35:41 +1100

    set suid to use luid to test

--- src/services/user/user/index.js
+++ src/services/user/user/index.js
@@ -1,5 +1,5 @@
 import { checkLocalUser, getLocalUserID, setLocalUserID } from './localUser';
-import { checkServerUser, getServerUserID, setServerUserID } from './serverUser';
+import { checkServerUser } from './serverUser';
 
 export const getLocalUser = () => {
   const userExists = checkLocalUser();
@@ -12,7 +12,9 @@ export const getLocalUser = () => {
 export const getServerUser = async (luid) => {
   const isUserSaved = checkServerUser();
 
-  const suid = isUserSaved ? getServerUserID() : await setServerUserID(luid);
+  console.log({ isUserSaved });
+
+  const suid = luid;
 
   return suid;
 };
