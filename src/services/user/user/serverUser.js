6f2df4081b6f60a9c2bacf30805d411c2e58d431
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-11-27 18:09:13 +1100

    bypass db.newUser(luid)

--- src/services/user/user/serverUser.js
+++ src/services/user/user/serverUser.js
@@ -1,11 +1,15 @@
-import db from '@/services/db';
+// import db from '@/services/db';
 
 export const getServerUserID = () => localStorage.getItem('suid');
 
 export const checkServerUser = () => getServerUserID() !== null;
 
 export const createServerUser = async (luid) => {
-  const { error, response } = await db.newUser(luid);
+  // const { error, response } = await db.newUser(luid);
+  const error = false;
+  const response = {
+    createUser: { _id: luid },
+  };
   const {
     createUser: { _id: id },
   } = response;
