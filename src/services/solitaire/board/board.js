25bf71131bf5329b2c43e51508043ccfe8f7a711
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-01-22 16:10:32 +1100

    check for any valid moves when dealing a new board and deal again if no moves are found

--- src/services/solitaire/board/board.js
+++ src/services/solitaire/board/board.js
@@ -1,10 +1,30 @@
 import { buildCards, dealCards, shuffleCards } from '../cards';
+import { checkHasMoves } from '../moves';
+// import fixture from '../../../../tests/fixtures/boards/noMoves.json';
+import fixture from '../../../../tests/fixtures/boards/aceOnlyMove.json';
 
-// eslint-disable-next-line import/prefer-default-export
-export const initBoardCards = ({ cards, rules }, toShuffle = true) => {
+export const initBoard = ({ cards, rules }, toShuffle = true) => {
   const deck = buildCards(cards);
   const shuffledDeck = shuffleCards(deck, toShuffle);
   const boardCards = dealCards(shuffledDeck, rules);
+
+  return boardCards;
+};
+
+export const setBoardCards = (boardCards, settings) => {
+  const foundationCards = [[], [], [], []];
+  const hasBoardMoves = checkHasMoves({ boardCards, foundationCards });
+
+  const cards = hasBoardMoves ? boardCards : setBoardCards(initBoard(settings));
+
+  return cards;
+};
+
+export const initBoardCards = (settings) => {
+  // const cards = initBoard(settings);
+  const { cards } = fixture;
+
+  const boardCards = setBoardCards(cards, settings);
 
   return boardCards;
 };
