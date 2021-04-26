da396e178f6802651f06ce87ef997957ed9801a7
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-04-26 16:25:01 +1000

    update graphql db connection to use mongodb instead

--- src/functions/graphql/db.js
+++ src/functions/graphql/db.js
@@ -1,18 +1,24 @@
-import fetch from 'node-fetch';
-import ApolloClient from 'apollo-boost';
+import { MongoClient } from 'mongodb';
 import 'dotenv/config';
 
-const { FAUNA_URL, FAUNA_ACCESS_TOKEN } = process.env;
+const { MONGOBD_URI, MONGODB_USER, MONGOBD_PASS, MONGODB_DB } = process.env;
+
+const uri = `mongodb+srv://${MONGODB_USER}:${MONGOBD_PASS}@${MONGOBD_URI}/test?retryWrites=true&w=majority`;
+
+let cachedDb = null;
 
 // eslint-disable-next-line import/prefer-default-export
-export const client = new ApolloClient({
-  uri: FAUNA_URL,
-  fetch,
-  request: (operation) => {
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${FAUNA_ACCESS_TOKEN}`,
-      },
-    });
-  },
-});
+export const client = async () => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const connection = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = connection.db(MONGODB_DB);
+
+  return cachedDb;
+};
