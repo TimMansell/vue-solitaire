a6c5dd6ffa0b5d2b11dbe0fe8d0d507d0fa41d1e
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-04-28 18:01:57 +1000

    create createUser action so it can be tested

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,15 +1,18 @@
 import user from '@/services/user';
 
 const actions = {
-  async initUser({ commit }) {
+  async initUser({ commit, dispatch }) {
     const luid = user.getLocalUser();
     const userExistsOnServer = await user.checkUserExistsOnServer(luid);
 
     if (!userExistsOnServer) {
-      await user.createUserOnServer(luid);
+      dispatch('createUser', luid);
     }
 
     commit('SET_USER_ID', luid);
+  },
+  async createUser(_, luid) {
+    await user.createUserOnServer(luid);
   },
 };
 
