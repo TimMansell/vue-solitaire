f9bb69a7e77865992a7b37cad937fe03bcf9ff8d
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-04-28 23:05:40 +1000

    use mockUid instead of hardcoded value

--- src/store/user/__tests__/getters.spec.js
+++ src/store/user/__tests__/getters.spec.js
@@ -2,8 +2,10 @@ import getters from '../getters';
 
 const { luid } = getters;
 
+const mockUid = 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7';
+
 const state = {
-  luid: 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7',
+  luid: mockUid,
 };
 
 describe('User Store', () => {
--- src/store/user/__tests__/mutations.spec.js
+++ src/store/user/__tests__/mutations.spec.js
@@ -2,9 +2,11 @@ import mutations from '../mutations';
 
 const { SET_USER_ID } = mutations;
 
+const mockUid = 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7';
+
 describe('User Store', () => {
   it('SET_USER_ID', () => {
-    const luid = 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7';
+    const luid = mockUid;
     const state = { luid };
 
     SET_USER_ID(state, luid);
