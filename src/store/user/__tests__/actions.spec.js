459efa2ae101873bd11ee397cf713485bbd1974b
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-04-28 21:56:17 +1000

    update user store unit tests

--- src/store/user/__tests__/actions.spec.js
+++ src/store/user/__tests__/actions.spec.js
@@ -2,18 +2,31 @@ import actions from '../actions';
 
 const { initUser } = actions;
 
-const mockLuid = 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7';
-const mockSuid = 123;
+const mockUid = 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7';
 
 const commit = jest.fn();
+const dispatch = jest.fn();
 
 jest.mock('@/services/user');
 
-describe('User', () => {
-  it('initUser', async () => {
-    await initUser({ commit });
+describe('User Store', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
-    expect(commit).toHaveBeenCalledWith('SET_USER_ID', mockLuid);
-    expect(commit).toHaveBeenCalledWith('SET_USER_SID', mockSuid);
+  it('should not create a new user', async () => {
+    localStorage.setItem('luid', mockUid);
+
+    await initUser({ commit, dispatch });
+
+    expect(dispatch).not.toHaveBeenCalledWith('createUser', mockUid);
+  });
+
+  it('should create a new user', async () => {
+    localStorage.setItem('luid', '123');
+
+    await initUser({ commit, dispatch });
+
+    expect(dispatch).toHaveBeenCalledWith('createUser', '123');
   });
 });
--- src/store/user/__tests__/mutations.spec.js
+++ src/store/user/__tests__/mutations.spec.js
@@ -1,8 +1,8 @@
 import mutations from '../mutations';
 
-const { SET_USER_ID, SET_USER_SID } = mutations;
+const { SET_USER_ID } = mutations;
 
-describe('User', () => {
+describe('User Store', () => {
   it('SET_USER_ID', () => {
     const luid = 'f5c6a829-f0da-4dfc-81a0-e6419f0163c7';
     const state = { luid };
@@ -10,14 +10,5 @@ describe('User', () => {
     SET_USER_ID(state, luid);
 
     expect(state.luid).toEqual(luid);
-  });
-
-  it('SET_USER_SID', () => {
-    const suid = 123;
-    const state = { suid };
-
-    SET_USER_SID(state, suid);
-
-    expect(state.suid).toEqual(suid);
   });
 });
