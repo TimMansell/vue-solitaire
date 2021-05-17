fc3dc2156a248b20b5d20566428cc30a6d0f5780
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-05-17 13:53:55 +1000

    update Pagination component to use vuejs-paginate

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,80 +1,47 @@
 <template>
-  <ul class="pagination">
-    <li>
-      <Button @click="displayPage(firstPage)">
-        First
-      </Button>
-    </li>
-    <li>
-      <Button @click="displayPage(previousPage)">
-        Previous
-      </Button>
-    </li>
-    <li v-for="index in pages" :key="index">
-      <Button @click="displayPage(index)">
-        {{ index }}
-      </Button>
-    </li>
-    <li>
-      <Button @click="displayPage(nextPage)">
-        Next
-      </Button>
-    </li>
-    <li>
-      <Button @click="displayPage(lastPage)">
-        Last
-      </Button>
-    </li>
-  </ul>
+  <Paginate
+    :page-count="pages"
+    :page-range="11"
+    :margin-pages="2"
+    :first-last-button="false"
+    :click-handler="displayPage"
+    :container-class="'pagination'"
+    :page-class="'pagination__page'"
+    :prev-class="'pagination__page pagination__page--outline'"
+    :next-class="'pagination__page pagination__page--outline'"
+    :active-class="'pagination__page--is-active'"
+    :disabled-class="'pagination__page--is-disabled'"
+    :break-view-class="'pagination__page--is-break'"
+  />
 </template>
 
 <script>
-import Button from '@/components/Button.vue';
+import Paginate from 'vuejs-paginate';
 
 export default {
   name: 'Pagination',
   components: {
-    Button,
+    Paginate,
   },
   props: {
     pages: {
       type: Number,
       default: 0,
     },
-  },
-  data() {
-    return {
-      page: 1,
-    };
-  },
-  computed: {
-    firstPage() {
-      return 1;
-    },
-    previousPage() {
-      const page = this.page - 1;
-
-      return page;
-    },
-    nextPage() {
-      const page = this.page + 1;
-
-      return page;
-    },
-    lastPage() {
-      return this.pages;
+    limit: {
+      type: Number,
+      default: 15,
     },
   },
   methods: {
     displayPage(page) {
-      this.page = page;
       this.$emit('page', page);
     },
   },
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .pagination {
   display: flex;
   flex-wrap: wrap;
@@ -82,5 +49,30 @@ export default {
   gap: var(--pd-xs);
   list-style-type: none;
   padding: 0;
+  margin: 0;
+
+  &__page {
+    border-radius: var(--bdr-radius-sm);
+    padding: var(--pd-sm);
+    line-height: 1;
+
+    &--outline {
+      border: 1px solid var(--bdr-secondary);
+    }
+
+    &--is-active {
+      background: var(--col-secondary-alt);
+    }
+
+    &--is-disabled {
+      a {
+        cursor: not-allowed;
+      }
+    }
+
+    &--is-break {
+      border: 0;
+    }
+  }
 }
 </style>
