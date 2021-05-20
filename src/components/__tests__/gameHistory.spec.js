dbb4ef22b8433071b34a8c0f2aae32c5e0ba481d
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-05-20 15:52:02 +1000

    GameHistory unit tests

---
+++ src/components/__tests__/gameHistory.spec.js
@@ -0,0 +1,55 @@
+import { shallowMount } from '@vue/test-utils';
+import GameHistory from '@/components/GameHistory.vue';
+
+const mocks = {
+  $store: { dispatch: jest.fn() },
+};
+
+const mockComputed = {
+  gameHistory: () => [
+    {
+      date: '2021-05-20T02:24:30.233Z',
+      won: false,
+      lost: false,
+      moves: 0,
+      time: 0,
+    },
+    {
+      date: '2021-05-20T02:24:14.157Z',
+      won: false,
+      lost: false,
+      moves: 0,
+      time: 0,
+    },
+    {
+      date: '2021-05-20T02:24:08.397Z',
+      won: false,
+      lost: false,
+      moves: 0,
+      time: 0,
+    },
+    {
+      date: '2021-05-14T12:16:17.268Z',
+      won: true,
+      lost: false,
+      moves: 2,
+      time: 0,
+    },
+  ],
+  userStats: () => ({
+    completed: 4,
+  }),
+};
+
+describe('GameHistory.vue', () => {
+  it('matches snapshot', () => {
+    const wrapper = shallowMount(GameHistory, {
+      mocks,
+      computed: {
+        ...mockComputed,
+      },
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
