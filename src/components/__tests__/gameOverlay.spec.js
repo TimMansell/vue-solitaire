add085ca07792384fe30dff5fcc3696c2589c42f
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-05-20 16:40:07 +1000

    update GameOverlay unit tests

--- src/components/__tests__/gameOverlay.spec.js
+++ src/components/__tests__/gameOverlay.spec.js
@@ -9,6 +9,15 @@ describe('GameOverlay.vue', () => {
   it('matches snapshot', () => {
     const wrapper = shallowMount(GameOverlay, {
       mocks,
+      propsData: {
+        showLogo: true,
+        btnClose: () => {},
+      },
+      slots: {
+        title: 'Title',
+        msg: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        buttons: 'Buttons',
+      },
     });
 
     expect(wrapper).toMatchSnapshot();
@@ -17,12 +26,21 @@ describe('GameOverlay.vue', () => {
   it('does not render logo', () => {
     const wrapper = shallowMount(GameOverlay, {
       mocks,
-      propsData: {
-        showLogo: false,
-      },
     });
 
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('[data-test="game-overlay-logo"]').exists()).toBe(
+      false
+    );
+  });
+
+  it('does not render a close button', () => {
+    const wrapper = shallowMount(GameOverlay, {
+      mocks,
+    });
+
+    expect(wrapper.find('[data-test="game-overlay-close"]').exists()).toBe(
+      false
+    );
   });
 
   it('renders an alternate overlay class', () => {
@@ -45,18 +63,5 @@ describe('GameOverlay.vue', () => {
     });
 
     expect(wrapper.classes()).toContain('game-overlay--centered');
-  });
-
-  it('renders text in slots', () => {
-    const wrapper = shallowMount(GameOverlay, {
-      mocks,
-      slots: {
-        title: 'title',
-        msg: 'msg',
-        buttons: 'buttons',
-      },
-    });
-
-    expect(wrapper).toMatchSnapshot();
   });
 });
