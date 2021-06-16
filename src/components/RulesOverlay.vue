a4418ed255dbdb1b1097693e54f6c81ef757004a
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-06-16 21:49:40 +1000

    remove alt prop from GameOverlay component usage

--- src/components/RulesOverlay.vue
+++ src/components/RulesOverlay.vue
@@ -1,5 +1,5 @@
 <template>
-  <GameOverlay alt :btn-close="toggleRules" data-test="rules-overlay">
+  <GameOverlay :btn-close="toggleRules" data-test="rules-overlay">
     <template #title>
       How to play
     </template>
