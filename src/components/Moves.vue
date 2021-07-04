b27df452d3840568181df20fe8367c5089d052cf
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-07-04 16:54:05 +1000

    create Moves component

---
+++ src/components/Moves.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Moves: {{ moves }}</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Moves',
+  computed: {
+    ...mapGetters(['moves']),
+  },
+};
+</script>
