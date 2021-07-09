cf050849236db0b65bc5d2b7d039f0d658605784
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-07-09 15:51:43 +1000

    create PortfolioButton

---
+++ src/components/PortfolioButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <Button
+    :is-stacked="isStacked"
+    @click="gotoUrl"
+    data-test="github-btn"
+    title="Visit my Portfolio"
+  >
+    <FontAwesomeIcon :icon="githubIcon" />
+  </Button>
+</template>
+
+<script>
+import { faGlobeAsia } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Button from './Button.vue';
+
+export default {
+  name: 'PortfolioButton',
+  components: {
+    Button,
+    FontAwesomeIcon,
+  },
+  props: {
+    isStacked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      githubIcon: faGlobeAsia,
+    };
+  },
+  methods: {
+    gotoUrl() {
+      window.open('https://timmansell.com', '_blank');
+    },
+  },
+};
+</script>
