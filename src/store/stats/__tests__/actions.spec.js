3a7b87f604bd9784c7e38e7784bc9aa41bb4104c
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-08-18 16:39:41 +1000

    update user store unit tests

--- src/store/stats/__tests__/actions.spec.js
+++ src/store/stats/__tests__/actions.spec.js
@@ -8,17 +8,18 @@ const mockStats = { won: 1, lost: 2, competed: 3 };
 const mockLeaderboardsMoves = [
   {
     rank: 1,
-    date: '2021-04-29T12:25:47.907Z',
-    uid: '7dac9d78-353f-409b-8a7f-2192409c44a2',
+    date: '29-04-2021',
+    player: 'Player 1',
     moves: 2,
   },
   {
     rank: 2,
-    date: '2021-04-29T12:26:20.825Z',
-    uid: '2cbf658a-3102-4e9d-b749-bac853efed0d',
+    date: '29-04-2021',
+    player: 'Player 2',
     moves: 2,
   },
 ];
+
 const rootState = {
   user: {
     luid: mockUid,
@@ -47,7 +48,7 @@ describe('Stats Store', () => {
   });
 
   it('getLeaderboards', async () => {
-    await getLeaderboards({ commit });
+    await getLeaderboards({ commit }, {});
 
     expect(commit).toHaveBeenCalledWith(
       'SET_LEADERBOARDS',
