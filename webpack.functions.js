fd3b150b0e8489939f16c55519cf6b223cecf88d
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-08-27 14:48:45 +1000

    add '@' alias for netlify-lambda

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,5 +1,11 @@
 const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = {
   externals: [nodeExternals()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
 };
