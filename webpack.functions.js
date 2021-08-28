f313de49a65529ca4f7940ba0e54b8b4f8530a73
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-08-28 12:07:17 +1000

    remove browser from package.json

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,11 +1,5 @@
 const nodeExternals = require('webpack-node-externals');
-const path = require('path');
 
 module.exports = {
   externals: [nodeExternals()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-    },
-  },
 };
