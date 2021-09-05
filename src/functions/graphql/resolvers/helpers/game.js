4ed86f61c6b428e91901a29b38c913f2dcdeaa0c
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-09-05 18:18:34 +1000

    calculate game time in graphql resolver

--- src/functions/graphql/resolvers/helpers/game.js
+++ src/functions/graphql/resolvers/helpers/game.js
@@ -1,5 +1,47 @@
+import { differenceInSeconds, parseISO } from 'date-fns';
 import { findAllItems, findItemInDb } from './db';
 import { validateGame } from '../../../../services/solitaire';
+
+export const calculateTime = (moves) => {
+  const pauseMoves = moves
+    .filter(({ type }) => type === 'pause')
+    .map(({ date }) => date);
+
+  const resumeMoves = moves
+    .filter(({ type }) => type === 'resume')
+    .map(({ date }) => date);
+
+  const cardMoves = moves
+    .filter(({ type }) => type === 'board' || type === 'foundation')
+    .map(({ date }) => date);
+
+  const pauseResumeMoves = pauseMoves.map((date, index) => ({
+    pause: date,
+    resume: resumeMoves[index],
+  }));
+
+  console.log({ pauseResumeMoves });
+
+  const [firstMove] = cardMoves;
+  const lastMove = cardMoves[cardMoves.length - 1];
+
+  const cardMoveTimes = differenceInSeconds(
+    parseISO(lastMove),
+    parseISO(firstMove)
+  );
+
+  const pauseMoveTimes = pauseResumeMoves.reduce(
+    (totalTime, { pause, resume }) =>
+      totalTime + differenceInSeconds(parseISO(resume), parseISO(pause)),
+    0
+  );
+
+  const gameTime = cardMoveTimes - pauseMoveTimes;
+
+  console.log({ gameTime, cardMoveTimes, pauseMoveTimes });
+
+  return gameTime;
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export const validateMoves = async (client, uid) => {
