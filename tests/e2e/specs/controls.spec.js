9d288427fda4036ad242d8ffa05bb9f45d25e429
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-09-14 13:02:07 +1000

    refactor controls tests to remove cy.setBoard()

--- tests/e2e/specs/controls.spec.js
+++ tests/e2e/specs/controls.spec.js
@@ -1,8 +1,9 @@
-import emptyColumn from '../../fixtures/boards/emptyColumn.json';
+import quitGameDeck from '../../fixtures/decks/quitGame.json';
+import quitGameMoves from '../../fixtures/moves/quitGame.json';
 
 describe('Controls', () => {
   beforeEach(() => {
-    cy.visitApp();
+    // cy.visitApp();
   });
 
   afterEach(() => {
@@ -10,48 +11,40 @@ describe('Controls', () => {
   });
 
   it('it should start a new game and reset board', () => {
-    cy.setBoard(emptyColumn).then(() => {
-      cy.get('[data-test="column-3"]').within(() => {
-        cy.get('[data-test="card-A♥"]').should('be.visible');
-      });
+    cy.visitApp({ mockDeck: quitGameDeck });
 
-      cy.get('[data-test="card-A♥"]').clickTo('[data-test="foundation-0"]');
+    cy.runGameWithClicks(quitGameMoves);
 
-      cy.get('[data-test="card-5♠"]').click();
+    cy.get('[data-test="card-7♠"]').click();
 
-      cy.newGame();
+    cy.newGame();
 
-      cy.wait('@waitForNewGameAPI');
+    cy.get('[data-test="foundation-0"]').shouldNotContain(['A♣']);
 
-      cy.get('[data-test="foundation-0"]').shouldNotContain(['A♥']);
-
-      cy.get('[data-card-selected="true"]').should('not.exist');
-    });
+    cy.get('[data-card-selected="true"]').should('not.exist');
   });
 
   it('it should continue current game', () => {
-    cy.setBoard(emptyColumn).then(() => {
-      cy.get('[data-test="column-3"]').within(() => {
-        cy.get('[data-test="card-A♥"]').should('be.visible');
-      });
+    cy.visitApp({ mockDeck: quitGameDeck });
 
-      cy.get('[data-test="card-A♥"]').clickTo('[data-test="foundation-0"]');
+    cy.runGameWithClicks(quitGameMoves);
 
-      cy.get('[data-test="card-5♠"]').click();
+    cy.get('[data-test="card-7♠"]').click();
 
-      cy.get('[data-test="new-game-btn"]').click();
+    cy.get('[data-test="new-game-btn"]').click();
 
-      cy.get('[data-test="game-overlay-btns"]').within(() => {
-        cy.get('[data-test="continue-game-btn"]').click();
-      });
-
-      cy.get('[data-test="foundation-0"]').shouldContain(['A♥']);
-
-      cy.get('[data-test="card-5♠"]').should('have.class', 'card--is-selected');
+    cy.get('[data-test="game-overlay-btns"]').within(() => {
+      cy.get('[data-test="continue-game-btn"]').click();
     });
+
+    cy.get('[data-test="foundation-0"]').shouldContain(['A♣']);
+
+    cy.get('[data-test="card-7♠"]').should('have.class', 'card--is-selected');
   });
 
   it('it should open and close rules', () => {
+    cy.visitApp();
+
     cy.get('[data-test="game-rules-btn"]').click();
 
     cy.get('[data-test="rules-overlay"]').should('be.visible');
@@ -62,6 +55,8 @@ describe('Controls', () => {
   });
 
   it('it should open and close history', () => {
+    cy.visitApp();
+
     cy.get('[data-test="history-btn"]').click();
 
     cy.get('[data-test="history-overlay"]').should('exist');
@@ -72,6 +67,8 @@ describe('Controls', () => {
   });
 
   it('it should pause a game and show game paused overlay', () => {
+    cy.visitApp();
+
     cy.get('[data-test="pause-game-btn"]').click();
 
     cy.get('[data-test="game-paused"]').should('be.visible');
@@ -84,12 +81,12 @@ describe('Controls', () => {
   });
 
   it('it should show correct summary on game overlay', () => {
-    cy.setBoard(emptyColumn).then(() => {
-      cy.get('[data-test="card-A♥"]').clickTo('[data-test="foundation-0"]');
+    cy.visitApp({ mockDeck: quitGameDeck });
 
-      cy.get('[data-test="pause-game-btn"]').click();
+    cy.runGameWithClicks(quitGameMoves);
 
-      cy.checkGameSummaryValues({ moves: 1 });
-    });
+    cy.get('[data-test="pause-game-btn"]').click();
+
+    cy.checkGameSummaryValues({ moves: 10 });
   });
 });
