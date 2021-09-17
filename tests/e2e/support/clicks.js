f30695fcf73908313b56cda155d72ca551df4224
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-09-17 17:27:19 +1000

    fix broken runGameWithClicks()

--- tests/e2e/support/clicks.js
+++ tests/e2e/support/clicks.js
@@ -11,15 +11,11 @@ Cypress.Commands.add('clickFromTo', (clickFrom, clickTo) => {
 Cypress.Commands.add('runGameWithClicks', (moves) => {
   moves.forEach(({ value, suit, selectedColumn, isBoard, isFoundation }) => {
     if (isBoard) {
-      cy.get(`[data-test="card-${value}${suit}"]`).clickTo(
-        `[data-test="column-${selectedColumn}"]`
-      );
+      cy.clickFromTo(`${value}${suit}`, `column-${selectedColumn}`);
     }
 
     if (isFoundation) {
-      cy.get(`[data-test="card-${value}${suit}"]`).clickTo(
-        `[data-test="foundation-${selectedColumn}"]`
-      );
+      cy.clickFromTo(`${value}${suit}`, `foundation-${selectedColumn}`);
     }
   });
 });
