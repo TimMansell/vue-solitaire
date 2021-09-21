be32988d4f601675e4414f5bc329a569e388d745
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-09-21 17:10:26 +1000

    update checkSelectedCard() to test [data-card-selected="true"] dynamically

--- tests/e2e/support/card.js
+++ tests/e2e/support/card.js
@@ -1,8 +1,9 @@
 Cypress.Commands.add('checkSelectedCard', ({ card, isSelected }) => {
   const testClass = isSelected ? 'have.class' : 'not.have.class';
+  const shouldExist = isSelected ? 'exist' : 'not.exist';
 
   cy.get(`[data-test="card-${card}"]`).should(testClass, 'card--is-selected');
-  cy.get('[data-card-selected="true"]').should('not.exist');
+  cy.get('[data-card-selected="true"]').should(shouldExist);
 });
 
 Cypress.Commands.add('checkCardPosition', ({ card, column, position }) => {
