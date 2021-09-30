84dfb2be994b2876a8583767bacb1adaf70b54d7
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-09-30 21:22:08 +1000

    create Stats component

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,87 +1,82 @@
 <template>
-  <div class="stats">
-    <div class="stats__item">
-      <UserGames />
-    </div>
-    <div class="stats__item">
-      <Moves />
-    </div>
-    <div class="stats__item">
-      <Timer />
-    </div>
+  <div>
+    <Table
+      :headings="['Played', 'Won', 'Lost', 'Gave Up']"
+      :items="userStatstics"
+      :placeholder-rows="2"
+      data-test="user-stats"
+    />
+    <h1>Global Stats</h1>
+    <Table
+      :headings="['Played', 'Won', 'Lost', 'Gave Up']"
+      :items="globalStatstics"
+      :placeholder-rows="2"
+      data-test="global-stats"
+    />
   </div>
 </template>
 
 <script>
-import UserGames from '@/components/UserGames.vue';
-import Timer from '@/components/Timer.vue';
-import Moves from '@/components/Moves.vue';
+import numeral from 'numeral';
+import { mapActions, mapGetters } from 'vuex';
+import Table from '@/components/Table.vue';
+
+export const calcPercent = (value) => numeral(value).format('0.00%');
+
+export const calcNumber = (value) => numeral(value).format('0,0');
+
+export const calcStats = ({ completed, won, lost }) => {
+  if (!completed && completed !== 0) {
+    return [];
+  }
+
+  const abandoned = completed - won - lost;
+
+  const completedCount = calcNumber(completed);
+  const wonCount = calcNumber(won);
+  const lostCount = calcNumber(lost);
+  const abandonedCount = calcNumber(abandoned);
+
+  const wonPercent = calcPercent(won / completed);
+  const lostPercent = calcPercent(lost / completed);
+  const abandonedPercent = calcPercent(abandoned / completed);
+
+  const stats = [
+    [completedCount, wonCount, lostCount, abandonedCount],
+    ['', wonPercent, lostPercent, abandonedPercent],
+  ];
+
+  return stats;
+};
 
 export default {
   name: 'Stats',
   components: {
-    UserGames,
-    Timer,
-    Moves,
+    Table,
+  },
+  computed: {
+    ...mapGetters(['userStats', 'globalStats']),
+    userStatstics() {
+      const { userStats } = this;
+      const stats = calcStats(userStats);
+
+      return stats;
+    },
+    globalStatstics() {
+      const { globalStats } = this;
+      const stats = calcStats(globalStats);
+
+      return stats;
+    },
+  },
+  methods: {
+    ...mapActions(['getStats', 'clearStats']),
+  },
+  mounted() {
+    this.getStats();
+  },
+  destroyed() {
+    this.clearStats();
   },
 };
 </script>
-
-<style scoped lang="scss">
-.stats {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  justify-content: space-between;
-  width: 100%;
-  font-size: var(--font-size-sm);
-  line-height: 1;
-  color: var(--text-primary);
-
-  @media (min-width: $bp-sm) {
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    gap: 0 var(--mg-sm);
-    order: -1;
-    width: auto;
-    line-height: 1.25;
-  }
-
-  @media (min-width: $bp-lg) {
-    font-size: var(--font-size);
-  }
-
-  &__item {
-    display: flex;
-
-    &:nth-child(1) {
-      @media (min-width: $bp-sm) {
-        grid-area: 1 / 1 / 2 / 2;
-      }
-    }
-
-    &:nth-child(2) {
-      justify-content: center;
-
-      @media (min-width: $bp-sm) {
-        grid-area: 2 / 2 / 3 / 3;
-        justify-content: flex-start;
-      }
-
-      &::before {
-        @media (min-width: $bp-sm) {
-          content: '/';
-          margin-right: var(--mg-sm);
-        }
-      }
-    }
-
-    &:nth-child(3) {
-      justify-content: flex-end;
-
-      @media (min-width: $bp-sm) {
-        grid-area: 2 / 1 / 3 / 2;
-      }
-    }
-  }
-}
-</style>
--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -2,86 +2,20 @@
   <GameOverlay center-content show-close data-test="stats-overlay">
     <template #title> Your Stats </template>
     <template #msg>
-      <Table
-        :headings="['Played', 'Won', 'Lost', 'Gave Up']"
-        :items="userStatstics"
-        :placeholder-rows="2"
-        data-test="user-stats"
-      />
-      <h1>Global Stats</h1>
-      <Table
-        :headings="['Played', 'Won', 'Lost', 'Gave Up']"
-        :items="globalStatstics"
-        :placeholder-rows="2"
-        data-test="global-stats"
-      />
+      <Stats />
     </template>
   </GameOverlay>
 </template>
 
 <script>
-import numeral from 'numeral';
-import { mapActions, mapGetters } from 'vuex';
 import GameOverlay from '@/components/GameOverlay.vue';
-import Table from '@/components/Table.vue';
-
-export const calcPercent = (value) => numeral(value).format('0.00%');
-
-export const calcNumber = (value) => numeral(value).format('0,0');
-
-export const calcStats = ({ completed, won, lost }) => {
-  if (!completed && completed !== 0) {
-    return [];
-  }
-
-  const abandoned = completed - won - lost;
-
-  const completedCount = calcNumber(completed);
-  const wonCount = calcNumber(won);
-  const lostCount = calcNumber(lost);
-  const abandonedCount = calcNumber(abandoned);
-
-  const wonPercent = calcPercent(won / completed);
-  const lostPercent = calcPercent(lost / completed);
-  const abandonedPercent = calcPercent(abandoned / completed);
-
-  const stats = [
-    [completedCount, wonCount, lostCount, abandonedCount],
-    ['', wonPercent, lostPercent, abandonedPercent],
-  ];
-
-  return stats;
-};
+import Stats from '@/components/Stats.vue';
 
 export default {
-  name: 'GameLost',
+  name: 'StatsPage',
   components: {
     GameOverlay,
-    Table,
-  },
-  computed: {
-    ...mapGetters(['userStats', 'globalStats']),
-    userStatstics() {
-      const { userStats } = this;
-      const stats = calcStats(userStats);
-
-      return stats;
-    },
-    globalStatstics() {
-      const { globalStats } = this;
-      const stats = calcStats(globalStats);
-
-      return stats;
-    },
-  },
-  methods: {
-    ...mapActions(['getStats', 'clearStats']),
-  },
-  mounted() {
-    this.getStats();
-  },
-  destroyed() {
-    this.clearStats();
+    Stats,
   },
 };
 </script>
