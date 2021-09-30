4b613fab810c8eaad6476a2b7a1edcc63afb38cc
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-09-30 20:11:51 +1000

    move router code to own file

---
+++ src/router/index.js
@@ -0,0 +1,112 @@
+import VueRouter from 'vue-router';
+import store from '@/store';
+import Home from '@/pages/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    components: {
+      main: Home,
+    },
+  },
+  {
+    path: '/new',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/New.vue'),
+    },
+  },
+  {
+    path: '/pause',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/Pause.vue'),
+    },
+  },
+  {
+    path: '/games',
+    redirect: '/games/1/25',
+  },
+  {
+    path: '/games/:page/:limit',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/Games.vue'),
+    },
+  },
+  {
+    path: '/stats',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/Stats.vue'),
+    },
+  },
+  {
+    path: '/leaderboards',
+    redirect: '/leaderboards/moves/25',
+  },
+  {
+    path: '/leaderboards/:showBest/:limit',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/Leaderboards.vue'),
+    },
+  },
+  {
+    path: '/rules',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/Rules.vue'),
+    },
+  },
+  {
+    path: '/won',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/Won.vue'),
+    },
+    beforeEnter(to, from, next) {
+      if (store.getters.hasGameWon) {
+        next();
+      } else {
+        next('/');
+      }
+    },
+  },
+  {
+    path: '/lost',
+    components: {
+      main: Home,
+      overlay: () => import('@/pages/Lost.vue'),
+    },
+    beforeEnter(to, from, next) {
+      if (store.getters.hasGameLost) {
+        next();
+      } else {
+        next('/');
+      }
+    },
+  },
+  {
+    path: '*',
+    components: {
+      main: () => import('@/pages/PageNotFound.vue'),
+    },
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/') {
+    store.dispatch('setGamePaused', false);
+  } else {
+    store.dispatch('setGamePaused', true);
+  }
+
+  next();
+});
+
+export default router;
