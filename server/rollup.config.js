422069180b810fe97bd84cf06c5875441bb5bdc3
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-10-04 18:04:17 +1100

    update node version to 14.14.0 & install rollup plugins to work with current codebase

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -1,4 +1,8 @@
-import { uglify } from 'rollup-plugin-uglify';
+import commonjs from '@rollup/plugin-commonjs';
+// import { terser } from 'rollup-plugin-terser';
+import alias from '@rollup/plugin-alias';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'main.js',
@@ -6,5 +10,13 @@ export default {
     file: 'index.js',
     format: 'cjs',
   },
-  plugins: [uglify()],
+  plugins: [
+    commonjs(),
+    nodeResolve({ preferBuiltins: true }),
+    json(),
+    // terser(),
+    alias({
+      entries: [{ find: '@', replacement: '../src' }],
+    }),
+  ],
 };
