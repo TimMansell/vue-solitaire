d1e519ef760f1ddd56dd178e95a432f71d0f0865
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-10-15 15:34:05 +1100

    remove saveGame, lostGame, quitGame from graphql

--- src/functions/graphql/resolvers.js
+++ src/functions/graphql/resolvers.js
@@ -1,5 +1,4 @@
 import { queries } from './resolvers/queries';
-import { mutations } from './resolvers/mutations';
 import { userStats } from './resolvers/userStats';
 import { globalStats } from './resolvers/globalStats';
 import { user } from './resolvers/user';
@@ -8,7 +7,6 @@ import { leaderboards } from './resolvers/leaderboards';
 // eslint-disable-next-line import/prefer-default-export
 export const resolvers = {
   Query: queries,
-  Mutation: mutations,
   UserStats: userStats,
   GlobalStats: globalStats,
   User: user,
