630e53f0b0d932a41075fb99c1876fe413ca891f
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-01-29 19:12:15 +1100

    remove deprecated OfflineAlert & OfflineMode

--- src/components/Alerts.vue
+++ src/components/Alerts.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
     <ConnectingAlert />
-    <OfflineAlert />
     <UpdatedAlert />
   </div>
 </template>
 
 <script>
 import ConnectingAlert from '@/components/ConnectingAlert.vue';
-import OfflineAlert from '@/components/OfflineAlert.vue';
 import UpdatedAlert from '@/components/UpdatedAlert.vue';
 
 export default {
   name: 'Alerts',
   components: {
     ConnectingAlert,
-    OfflineAlert,
     UpdatedAlert,
   },
 };
