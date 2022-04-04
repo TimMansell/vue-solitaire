d9a0e419d6d1f0c89d2c665dbdc606fc005ba5fb
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-04-04 11:08:53 +1000

    update oldVersion checker to use semver package

--- src/services/version/helpers.js
+++ src/services/version/helpers.js
@@ -1,13 +1,10 @@
+import { valid, lt } from 'semver';
 import { version } from '../../../package.json';
 
 export const getOldVersions = () => {
   const keys = Object.keys(localStorage);
-
   const legacyVersion = keys.filter((key) => key.includes('vuex'));
-
-  const oldVersion = keys.filter(
-    (key) => !key.includes(version) && key.includes('.')
-  );
+  const oldVersion = keys.filter((key) => valid(key) && lt(key, version));
 
   return [...legacyVersion, ...oldVersion];
 };
