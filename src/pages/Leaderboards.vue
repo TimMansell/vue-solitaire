19d44e6cc3ec5e26a4af44dd8002ce876b641cdf
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-05-26 14:52:09 +1000

    remove vue-scrollto from Leaderboards.vue

--- src/pages/Leaderboards.vue
+++ src/pages/Leaderboards.vue
@@ -7,13 +7,12 @@
   >
     <template #title> Leaderboards </template>
     <template #msg>
-      <Leaderboards @scrollTo="scrollTo" />
+      <Leaderboards />
     </template>
   </GameOverlay>
 </template>
 
 <script>
-import VueScrollTo from 'vue-scrollto';
 import GameOverlay from '@/components/GameOverlay.vue';
 import Leaderboards from '@/components/Leaderboards.vue';
 
@@ -22,16 +21,6 @@ export default {
   components: {
     GameOverlay,
     Leaderboards,
-  },
-  methods: {
-    scrollTo(elem) {
-      const { leaderboardOverlay } = this.$refs;
-
-      VueScrollTo.scrollTo(elem, {
-        container: leaderboardOverlay.$el,
-        offset: -10,
-      });
-    },
   },
 };
 </script>
