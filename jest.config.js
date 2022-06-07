fbc8475dec582601cce301d7efc7ec59198cfb9c
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-06-07 19:31:36 +1000

    update jest.config.js to use vue3 packages

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,15 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'vue'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^#/(.*)$': '<rootDir>/$1',
   },
   modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
-  snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)'],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
       'jest-transform-stub',
-    '^.+\\.vue$': '@vue/vue2-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
   },
   transformIgnorePatterns: ['/node_modules/'],
 };
