3972a2dc936e3e46bd6e6b9e23622d7c1560c656
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-07-05 13:17:52 +1000

    add ConnectionError to main bundle as it can't load dynamically if network is down

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,13 @@ import Pause from '@/pages/Pause.vue';
 import History from '@/pages/History.vue';
 import Stats from '@/pages/Stats.vue';
 import Leaderboards from '@/pages/Leaderboards.vue';
+import ConnectionError from '@/pages/ConnectionError.vue';
 
 const Won = () => import('@/pages/Won.vue');
 const Lost = () => import('@/pages/Lost.vue');
 const Rules = () => import('@/pages/Rules.vue');
 const Update = () => import('@/pages/Update.vue');
 const PageNotFound = () => import('@/pages/PageNotFound.vue');
-const ConnectionError = () => import('@/pages/ConnectionError.vue');
 
 const routes = [
   {
